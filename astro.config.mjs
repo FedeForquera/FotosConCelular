d62b15eee059861c7db2e1b9dec37b624a82c70d
Pablo Proboste <Pabloproboste64@gmail.com>
2026-05-07 14:52:57 -0300

    ajuste de loop en verscel.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.fedeforquera.com.ar',
+  site: 'https://fedeforquera.com.ar',
 
   vite: {
     plugins: [tailwindcss()]
